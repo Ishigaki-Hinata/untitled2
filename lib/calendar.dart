805b131939aa2ff625ce9749bd3d4152399059f5
hinut <si19003@edu.aomori-u.ac.jp>
2022-08-06 00:53:10 +0900

    最初のコミット

---
+++ lib/calendar.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'dart:convert';
+import 'dart:async';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Sample',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(
+          title: 'Flutter Demo Home Page',
+      ),
+    );
+  }
+}
+
+class Event {
+  Event(this.eventName, this.from, this.to, this.background, this.isAllDay);
+  String eventName;
+  DateTime from;
+  DateTime to;
+  Color background;
+  bool isAllDay;
+}
+
+class EventDataSource extends CalendarDataSource {
+  EventDataSource(List<Event> event) {
+    appointments = event;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return appointments![index].from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return appointments![index].to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return appointments![index].eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return appointments![index].background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].isAllDay;
+  }
+}
+
+List<Event> _getDataSource() {
+  final List<Event> event = <Event>[];
+  final DateTime today = DateTime.now();
+  final DateTime startTime =
+  DateTime(today.year, today.month, today.day, 9, 0, 0);
+  final DateTime endTime = startTime.add(const Duration(hours: 2));
+  event.add(
+      Event('イベント', startTime, endTime, const Color(0xFF0F8644), false));
+  return event;
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+/*TimeOfDay? selectedTime;
+Future _pickTime(BuildContext context)async{
+  final initialTime =TimeOfDay(hour: 12, minute: 0);
+
+  //時計表示
+  final newTime=
+      await showTimePicker(context: context, initialTime: initialTime);
+  if(newTime !=null) {
+    setState(() => selectedTime = newTime);
+  }else {
+    return;
+  }
+
+  }*/
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+        actions: [
+        ],
+      ),
+      body: Center(
+          child:SfCalendar(
+            todayHighlightColor: Colors.red,
+            allowedViews:const [CalendarView.month,CalendarView.week,CalendarView.day,],
+            monthViewSettings: MonthViewSettings(appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
+            dataSource: EventDataSource(_getDataSource()),
+            onTap: (CalendarTapDetails details) {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Container(
+                        child: Text("${details.date}"),
+                      ),
+                      actions: <Widget>[
+                        new TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('Close'))
+                      ],
+                    );
+                  });
+            },
+          ),
+      ),
+    );
+  }
+}
+
