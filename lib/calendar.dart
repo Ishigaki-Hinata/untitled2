cf454cdb2774aea003466389ed1c64729e4e5703
hinut <si19003@edu.aomori-u.ac.jp>
2022-09-13 16:29:42 +0900

    不要な文章の削除

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+/*import 'package:flutter_localizations/flutter_localizations.dart';
 import 'dart:convert';
-import 'dart:async';
+import 'dart:async';*/
 void main() {
   runApp(const MyApp());
 }
@@ -16,15 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Sample',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(
@@ -73,52 +64,25 @@ class EventDataSource extends CalendarDataSource {
     return appointments![index].isAllDay;
   }
 }
-
+//カレンダーのイベント出力
 List<Event> _getDataSource() {
   final List<Event> event = <Event>[];
   final DateTime today = DateTime.now();
   final DateTime startTime =
-  DateTime(today.year, today.month, today.day, 9, 0, 0);
-  final DateTime endTime = startTime.add(const Duration(hours: 2));
+  DateTime(today.year, today.month, today.day, 9, 0, 0);//今の年月日,時間
+  final DateTime endTime = startTime.add(const Duration(hours: 2));//二時間分
   event.add(
-      Event('イベント', startTime, endTime, const Color(0xFF0F8644), false));
+      Event('イベント', startTime, endTime, const Color(0xFF0F8644), false));//イベント出力
   return event;
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
-/*TimeOfDay? selectedTime;
-Future _pickTime(BuildContext context)async{
-  final initialTime =TimeOfDay(hour: 12, minute: 0);
-
-  //時計表示
-  final newTime=
-      await showTimePicker(context: context, initialTime: initialTime);
-  if(newTime !=null) {
-    setState(() => selectedTime = newTime);
-  }else {
-    return;
-  }
-
-  }*/
 
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -137,16 +101,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         actions: [
         ],
